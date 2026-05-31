faff6b9e04608f85381072e1daf9d36e7584d6e6
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2026-05-31 22:14:12 +0200

    fix: add init-mongo.sh script to ensure UniFi user is created in MongoDB

---
+++ network/unifi-controller/init-mongo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+mongosh --quiet -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin <<EOF
+  db = db.getSiblingDB("$MONGO_INITDB_DATABASE");
+  db.createUser({
+    user: "$MONGO_INITDB_ROOT_USERNAME",
+    pwd: "$MONGO_INITDB_ROOT_PASSWORD",
+    roles: [{ role: "dbOwner", db: "$MONGO_INITDB_DATABASE" }]
+  });
+EOF
